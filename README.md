c880f0879ecc263740d3a1b9bec326508081c154
Haroon Rasheed <haroon.rasheed@gigamon.com>
2021-11-01 00:24:50 -0700

    Fixed support for k8s version installation and readme update

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # kubernetes
+This script will help you install Kubernetes with different k8s versio, CNIs and Container runtime.
+
+I will be adding more CNIs in future.
+
+# Example usage:
+
+```
+./k8s_installer.sh --ver=1.22 --cri=containerd --net=calico --role=master
+
+```
--- k8s_installer.sh
+++ k8s_installer.sh
@@ -49,7 +49,7 @@ sudo apt -y install curl apt-transport-https
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt update
-sudo apt -y install vim git curl wget kubelet kubeadm kubectl
+sudo apt -y install vim git curl wget kubelet=${ver}.1-00 kubeadm=${ver}.1-00 kubectl=${ver}.1-00
 sudo apt-mark hold kubelet kubeadm kubectl
 kubectl version --client && kubeadm version
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
